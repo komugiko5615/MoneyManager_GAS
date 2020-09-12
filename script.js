cc9e86cf33c00cb17361d79d65747e24300d5856
komugiko5615 <zk24black@gmail.com>
2020-09-12 11:31:57 +0900

    初期ファイル追加

---
+++ script.js
@@ -0,0 +1,11 @@
+function getGetsumatsuZandaka() {
+  var yearMonth = inputYearMonth();
+  var gesshoZandaka = getGesshoZandaka(yearMonth);
+  var shuushi = getShuushi(yearMonth);
+  var
+  
+}
+
+function inputYearMonth() {
+  return '202009'
+}
