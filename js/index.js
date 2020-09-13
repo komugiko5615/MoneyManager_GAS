e4ed6fce48eb06e7a1c2b4635f466cc0b3584607
komugiko5615 <zk24black@gmail.com>
2020-09-13 19:03:37 +0900

    js修正

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(function(){
     $('#search').on('click', function() {
         google.script.run
-            .WithSuccessHandler( data => {
+            .withSuccessHandler( data => {
                 // 取得成功
                 console.log('取得成功：' + JSON.parse(data));
             })
-            .WithFailureHandler( data => {
+            .withFailureHandler( data => {
                 // 取得失敗
                 console.log('取得失敗:' + data);
             })
