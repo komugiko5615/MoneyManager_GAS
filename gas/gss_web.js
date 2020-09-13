f9a23d0bc2f725a5fe7a851f7c3233ae76929067
komugiko5615 <zk24black@gmail.com>
2020-09-13 17:23:04 +0900

    登録画面作成

--- gas/gss_web.js
+++ gas/gss_web.js
@@ -17,5 +17,7 @@ function doGet(e) {
                 break;
         }
     }
-    return HtmlService.createTemplateFromFile(template).evaluate();
+    // return HtmlService.createTemplateFromFile(template).evaluate();
+    // return HtmlService.createHtmlOutputFromFile(template).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    return HtmlService.createTemplateFromFile(template).evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
