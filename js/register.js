ecc258351af0583c93b5f4d180255968e0cb3b49
komugiko5615 <zk24black@gmail.com>
2020-09-14 00:26:14 +0900

    日付の形式変換処理作成

--- js/register.js
+++ js/register.js
@@ -1,11 +1,18 @@
 $(function(){
+    $('#shuushi_id').val("1");
+    $('#kouza_id').val("1");
+    $('#date').val("2020-09-01");
+    $('#shuushi_name').val("test");
+    $('#shuushi_type').val("1");
+    $('#kingaku').val(9999);
+
     $('#register_btn').on('click', function() {
         let data = [];
         let i = 0;
         data[i] = {};
         data[i].shuushi_id = $('#shuushi_id').val();
         data[i].kouza_id = $('#kouza_id').val();
-        data[i].date = $('#date').val();
+        data[i].date = $('#date').val().replace(/-/g, '');
         data[i].shuushi_name = $('#shuushi_name').val();
         data[i].shuushi_type = $('#shuushi_type').val();
         data[i].kingaku = $('#kingaku').val();
