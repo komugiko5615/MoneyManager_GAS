64347abcb83bff1c0a3a5de300f3808c7e683c21
komugiko5615 <zk24black@gmail.com>
2020-09-13 21:01:16 +0900

    テーブル出力部実装

--- js/index.js
+++ js/index.js
@@ -3,7 +3,11 @@ $(function(){
         google.script.run
             .withSuccessHandler( data => {
                 // 取得成功
-                console.log('取得成功：' + JSON.parse(data));
+                console.log('取得成功：' + data);
+                let tableData = JSON.parse(data);
+
+                renderTable(tableData);
+
             })
             .withFailureHandler( data => {
                 // 取得失敗
@@ -12,3 +16,35 @@ $(function(){
             .getMoneyData();
     });
 });
+
+let renderTable = function (data) {
+    let element = $('#main_table');
+
+    const kouzaData = data["口座マスター"];
+
+    // テーブル初期化
+    element.empty();
+
+    // ヘッダ部出力
+    let th = '<thead>';
+    th += '<tr>';
+    for(let key in kouzaData[0] ) {
+        th += '<th>' + key + '</th>';
+    }
+    th += '</tr>';
+    th += '</thead>';
+    element.append(th);
+
+    // データ部出力
+    let tbody = '<tbody>';
+    kouzaData.forEach( dataRow => {
+        tbody += '<tr>';
+        for(let key in dataRow) {
+            tbody += '<td>' + dataRow[key] + '</td>';
+        }
+        tbody += '</tr>';
+    });
+    tbody += '</tbody>';
+
+    element.append(tbody);
+}
