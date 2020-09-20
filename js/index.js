39997517d2a8da6b418c1853ccf7dfaf520b3adb
komugiko5615 <zk24black@gmail.com>
2020-09-20 20:38:49 +0900

    残高取得処理作成・データ表示タイミング修正

--- js/index.js
+++ js/index.js
@@ -1,20 +1,79 @@
 $(function(){
-    $('#search').on('click', function() {
-        google.script.run
-            .withSuccessHandler( data => {
-                // 取得成功
-                let tableData = JSON.parse(data);
-
-                renderTable(tableData);
-
-            })
-            .withFailureHandler( data => {
-                // 取得失敗
-                console.log('取得失敗:' + data);
-            })
-            .getTableData("収支");
-    });
+    fetchShuushiData();
+    fetchGesshoZandaka();
+    fetchGetsumatsuZandaka();
+    // test();
 });
+
+let test = function(){
+    // テーブルとボタン表示
+    $('#main_table').removeClass('hidden');
+    $('.btn-add-shuushi').removeClass('hidden');
+    $('.area-zandaka-start').removeClass('hidden');
+    $('.area-zandaka-end').removeClass('hidden');
+}
+
+// 月初残高取得
+let fetchGesshoZandaka = function() {
+    google.script.run
+    .withSuccessHandler( data => {
+        // 取得成功
+        let tableData = JSON.parse(data);
+
+        // データセット
+        $('.area-zandaka-start').find('.zandaka-kingaku').text(tableData[0].kingaku);
+        // データ表示
+        $('.area-zandaka-start').removeClass('hidden');
+
+    })
+    .withFailureHandler( data => {
+        // 取得失敗
+        console.log('取得失敗:' + data);
+    })
+    .getTableData("月初残高");
+
+}
+// 月末予測残高取得
+let fetchGetsumatsuZandaka = function() {
+    google.script.run
+    .withSuccessHandler( data => {
+        // 取得成功
+        let tableData = JSON.parse(data);
+
+        // データセット
+        $('.area-zandaka-end').find('.zandaka-kingaku').text(tableData[0].kingaku);
+        // データ表示
+        $('.area-zandaka-end').removeClass('hidden');
+
+    })
+    .withFailureHandler( data => {
+        // 取得失敗
+        console.log('取得失敗:' + data);
+    })
+    .getTableData("予測月末残高");
+
+}
+
+// 収支データ取得
+let fetchShuushiData = function() {
+    google.script.run
+    .withSuccessHandler( data => {
+        // 取得成功
+        let tableData = JSON.parse(data);
+
+        renderTable(tableData);
+
+        // テーブルとボタン表示
+        $('#main_table').removeClass('hidden');
+        $('.btn-add-shuushi').removeClass('hidden');
+
+    })
+    .withFailureHandler( data => {
+        // 取得失敗
+        console.log('取得失敗:' + data);
+    })
+    .getTableData("収支");
+}
 
 let renderTable = function (data) {
 
